c2f91cbc8f605818d4fd8f0ed9b2b17c8b67df6b
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-14 22:20:59 +0200

    Adding lesson 1

--- Lesson01ForLoops/Program.cs
+++ Lesson01ForLoops/Program.cs
@@ -10,6 +10,18 @@ namespace Lesson01ForLoops
     {
         static void Main(string[] args)
         {
+            for (int counter = 0; counter < 50; counter += 5)
+            {
+                Console.WriteLine(counter);
+            }
+           
+
+            // Odd numbers only
+            for (int i = 1; i < 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
         }
     }
 }
