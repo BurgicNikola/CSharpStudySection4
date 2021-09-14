de65850a79ac535d532a0ea0bb73f15d8399cd03
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-14 22:38:48 +0200

    Adding lesson 2

---
+++ Lesson02DoWhileLoop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter < 5);
+
+            Console.WriteLine("--------------------");
+
+            counter = 15;
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter < 5);
+
+            Console.WriteLine("--------------------");
+            string allNames = "";
+            int allNamesLength = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter a name of a friend: ");
+                string friendName = Console.ReadLine();
+                int nameLength = friendName.Length;
+                allNames += friendName + " ";
+                allNamesLength += nameLength;
+            } while (allNamesLength < 30);
+            Console.WriteLine("Thanks, that was enough. Names: " + allNames);
+
+            Console.Read();
+        }
+    }
+}
