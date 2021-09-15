35fab6decdb33b80042d34ef0219152ec00ce635
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-15 12:34:43 +0200

    Adding lesson 3

---
+++ Lesson03WhileLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while(counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.WriteLine("----------------------------");
+
+            // By pressing enter this program counts how many students have entered the class. There are 30 students that should attend the class.
+            counter = 0;
+            Console.WriteLine("Press enter for every student that enters the class.");
+            while (counter < 30)
+            {
+                Console.ReadLine();
+                counter++;
+                Console.Write(counter);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
